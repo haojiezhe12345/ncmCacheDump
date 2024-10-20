6ec7a2e8e3a9e347c79857634c446665a1d19317
浩劫者12345 <3112611479@qq.com>
2024-10-21 05:10:49 +0800

    add support for .nmsf

--- convert.py
+++ convert.py
@@ -25,6 +25,86 @@ def convert_uc(src: str, dest: str):
                 for b in buf:
                     buf1.append(b ^ 0xa3)
                 fw.write(buf1)
+    mtime = os.path.getmtime(src)
+    os.utime(dest, (mtime, mtime))
+
+
+def convert_file(src_file: str, out_dir: str, song_detail: dict = None):
+    print(src_file)
+
+    src_ext = os.path.splitext(src_file)[1]
+    src_name = {
+        '.uc': os.path.splitext(src_file)[0],
+        '.uc!': os.path.splitext(src_file)[0],
+        '.nmsf': os.path.splitext(src_file)[0].removesuffix('_0'),
+    }[src_ext]
+
+    size_matched = True
+    try:
+        with open(src_name + {
+            '.uc': '.idx',
+            '.uc!': '.idx!',
+            '.nmsf': '.nmsfi',
+        }[src_ext]) as f:
+            idx = json.load(f)
+            if os.path.getsize(src_file) != int(idx[{
+                '.uc': 'size',
+                '.uc!': 'filesize',
+                '.nmsf': 'file_size',
+            }[src_ext]]):
+                size_matched = False
+                print('Size not match')
+    except:
+        print('Unable to check file size')
+
+    ext = 'mp3'
+    try:
+        with open(src_name + {
+            '.uc': '.info',
+            '.uc!': '.idac!',
+            '.nmsf': '.config',
+        }[src_ext]) as f:
+            info = json.load(f)
+            ext = info[{
+                '.uc': 'format',
+                '.uc!': 'audioFormat',
+                '.nmsf': 'audioFormat',
+            }[src_ext]]
+    except:
+        print(f'Unable to determine file format, assuming it is .{ext}')
+
+    bitrate = SongFilename(src_file).bitrate
+
+    out_file = f"{song_detail['name']} - {song_detail['artists'][0]['name']}" if song_detail and song_detail['name'] else os.path.basename(src_file)
+    out_file = re.sub(r'[\\/:*?"<>|]', '_', out_file)
+    out_file += f' ({bitrate}k)' if size_matched else f' ({bitrate}k UNCOMPLETED)'
+    out_file += f'.{ext}'
+    out_file = os.path.join(out_dir, out_file)
+
+    convert_uc(src_file, out_file)
+
+
+class SongFilename:
+    def __init__(self, filename: str):
+        self.name = os.path.basename(os.path.splitext(filename)[0])
+        self.ext = os.path.splitext(filename)[1]
+        self.name_spilt = self.name.split({
+            '.uc': '-',
+            '.uc!': '-',
+            '.nmsf': '_',
+        }[self.ext])
+
+    @property
+    def id(self):
+        return int(self.name_spilt[0])
+
+    @property
+    def bitrate(self):
+        return round(int(self.name_spilt[1]) / {
+            '.uc': 1,
+            '.uc!': 1000,
+            '.nmsf': 1000,
+        }[self.ext])
 
 
 def get_song_details(song_ids: list[int]) -> list[dict]:
@@ -39,55 +119,26 @@ def get_song_details(song_ids: list[int]) -> list[dict]:
     return songs
 
 
-def convert_file(src_file: str, out_dir: str, song_detail: dict = None):
-    print(src_file)
-
-    is_mobile = src_file.endswith('!')
-
-    size_matched = True
-    try:
-        with open(os.path.splitext(src_file)[0] + ('.idx' if not is_mobile else '.idx!')) as f:
-            idx = json.load(f)
-            if os.path.getsize(src_file) != int(idx['size'] if not is_mobile else idx['filesize']):
-                size_matched = False
-                print('size not match')
-    except:
-        print('idx not found, not checking file size')
-
-    ext = 'mp3'
-    try:
-        with open(os.path.splitext(src_file)[0] + ('.info' if not is_mobile else '.idac!')) as f:
-            info = json.load(f)
-            ext = info['format'] if not is_mobile else info['audioFormat']
-    except:
-        print(f'info not found, assuming format is {ext}')
-
-    bitrate = int(os.path.basename(src_file).split('-')[1])
-    if is_mobile:
-        bitrate //= 1000
-
-    out_file = f"{song_detail['name']} - {song_detail['artists'][0]['name']}" if song_detail and song_detail['name'] else os.path.basename(src_file)
-    out_file = re.sub(r'[\\/:*?"<>|]', '_', out_file)
-    out_file += f' ({bitrate}k)' if size_matched else f' ({bitrate}k uncompleted)'
-    out_file += f'.{ext}'
-    out_file = os.path.join(out_dir, out_file)
-
-    convert_uc(src_file, out_file)
-
-
 def convert_folder(src_dir: str, out_dir='output', workers=WORKERS):
     t0 = time.time()
     print(f'Workers: {workers}')
 
-    conv_list = [file for file in os.listdir(src_dir) if file.endswith('.uc') or file.endswith('.uc!')]
+    conv_list: list[str] = []
+    for root, dirs, files in os.walk(src_dir):
+        for file in files:
+            filepath = os.path.join(root, file)
+            if os.path.splitext(file)[1] in ['.uc', '.uc!', '.nmsf']:
+                conv_list.append(filepath)
+                print(f'Added {len(conv_list)} songs', end='\r')
+    print()
 
     songs = {}
-    for song in get_song_details([int(file.split('-')[0]) for file in conv_list]):
+    for song in get_song_details([SongFilename(file).id for file in conv_list]):
         songs[song['id']] = song
 
     with ProcessPoolExecutor(max_workers=workers) as exe:
         for file in conv_list:
-            exe.submit(convert_file, os.path.join(src_dir, file), out_dir, songs.get(int(file.split('-')[0]), None))
+            exe.submit(convert_file, file, out_dir, songs.get(SongFilename(file).id, None))
         print('Task creation finished')
 
     t1 = time.time()
